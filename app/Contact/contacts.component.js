edba1a466c7ffee0ca721ad81b9e418abcf1ad69
vipul dahale <vipul.dahale@impetus.co.in>
2016-08-05 19:02:19 +0530

    Grid changes

--- app/Contact/contacts.component.js
+++ app/Contact/contacts.component.js
@@ -12,16 +12,66 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  * Created by abc on 17-Jul-16.
  */
 var core_1 = require('@angular/core');
+var main_1 = require('ag-grid-ng2/main');
 var ContactsComponent = (function () {
     function ContactsComponent() {
+        this.iteratorI = 0;
+        this.testTwoWay = "nothing";
+        this.gridOptions = {};
+        this.gridOptions.rowHeight = 120;
+        this.gridOptions.enableSorting = true;
+        this.gridOptions.enableFilter = true;
+        this.gridOptions.quickFilterText = this.searchText;
+        this.rowData = [
+            { name: "Tari", address: "Vijay Nagar", number: "+91568985", email: "tar@upd" },
+            { name: "krati", address: "Nanda Nagar", number: "+96585", email: "kj@69" },
+            { name: "ashish", address: "AB Road", number: "+32656", email: "psycho@sharma" },
+            { name: "jd", address: "Gaav", number: "+565656", email: "ddddddd" },
+        ];
+        this.columnDefs = [
+            {
+                headerName: 'Details', field: 'userDetail.name', sort: 'asc', comparator: this.compareDetails, filter: 'text',
+                filterParams: { apply: true, newRowsAction: 'keep' },
+                cellRenderer: function (params, index) {
+                    return "<div><span>" + params.data.name + "</span><br>\n                    <span>" + params.data.address + "</span><br>\n                    <span>" + params.data.number + "</span><br>\n                    <span>" + params.data.email + "</span></div>";
+                }
+            },
+            {
+                headerName: 'Action', sort: 'asc',
+                cellRenderer: function (params, index) {
+                    return "<span><button (click)=\"console.log('dd')\" >Read</button></span>\n                                                  <span><button (click)=\"deleteContact()\">Delete</button></span>";
+                }
+            }
+        ];
     }
-    ContactsComponent.prototype.ngOnInit = function () {
-        console.info("Heyoo..!! Am loaded");
+    ContactsComponent.prototype.readContact = function () {
+        console.log("read");
+    };
+    ContactsComponent.prototype.deleteContact = function () {
+        console.log("delete");
+    };
+    ContactsComponent.prototype.searchGrid = function (value) {
+        this.searchText = value;
+        /*console.log(this.searchText);
+        this.iteratorI++;
+        console.log("updateing");
+        this.rowData.push({name: "Tarsdfdasf", address: "Vijay asdfdsafNagar", number: "+9156sdf8985", email: "tar@dfsafupd"});
+        this.testTwoWay="new"+this.iteratorI;*/
+    };
+    ContactsComponent.prototype.compareDetails = function (valueA, valueB, rowA, rowB, inverter) {
+        if (rowA.data.name.toLowerCase() > rowB.data.name.toLowerCase()) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
     };
     ContactsComponent = __decorate([
         core_1.Component({
+            moduleId: module.id,
             selector: 'contact-grid',
-            template: "<h1>This is the contact view</h1>"
+            templateUrl: 'contact.component.html',
+            directives: [main_1.AgGridNg2]
         }), 
         __metadata('design:paramtypes', [])
     ], ContactsComponent);
--- app/Contact/contacts.component.ts
+++ app/Contact/contacts.component.ts
@@ -1,17 +1,89 @@
 /**
  * Created by abc on 17-Jul-16.
  */
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {OnInit} from '@angular/core';
+import {AgGridNg2} from 'ag-grid-ng2/main';
+import {GridOptions} from 'ag-grid/main';
 
 @Component({
-    selector:'contact-grid',
-    template:`<h1>This is the contact view</h1>`
+    moduleId: module.id,
+    selector: 'contact-grid',
+    templateUrl: 'contact.component.html',
+    directives: [AgGridNg2]
 })
 
-export class ContactsComponent implements OnInit{
-    ngOnInit() {
-    console.info("Heyoo..!! Am loaded");
+export class ContactsComponent {
+
+    private rowData;
+    private columnDefs;
+    private gridOptions;
+    private testTwoWay;
+    private iteratorI=0;
+    private searchText:string;
+    constructor() {
+        this.testTwoWay="nothing";
+        this.gridOptions = <GridOptions>{};
+        this.gridOptions.rowHeight = 120;
+        this.gridOptions.enableSorting = true;
+        this.gridOptions.enableFilter = true;
+        this.gridOptions.quickFilterText=this.searchText;
+        this.rowData = [
+            {name: "Tari", address: "Vijay Nagar", number: "+91568985", email: "tar@upd"},
+            {name: "krati", address: "Nanda Nagar", number: "+96585", email: "kj@69"},
+            {name: "ashish", address: "AB Road", number: "+32656", email: "psycho@sharma"},
+            {name: "jd", address: "Gaav", number: "+565656", email: "ddddddd"},
+        ];
+
+        this.columnDefs = [
+            {
+                headerName: 'Details', field: 'userDetail.name', sort: 'asc', comparator: this.compareDetails, filter: 'text',
+                filterParams: {apply: true, newRowsAction: 'keep'},
+                cellRenderer: function (params:any, index:any) {
+                    return `<div><span>${params.data.name}</span><br>
+                    <span>${params.data.address}</span><br>
+                    <span>${params.data.number}</span><br>
+                    <span>${params.data.email}</span></div>`
+                }
+            },
+
+            {
+                headerName: 'Action', sort: 'asc',
+                cellRenderer: function (params:any, index:any) {
+                    return `<span><button (click)="console.log('dd')" >Read</button></span>
+                                                  <span><button (click)="deleteContact()">Delete</button></span>`
+                }
+
+            }
+
+        ];
+
+    }
+
+    readContact() {
+        console.log("read");
+    }
+
+    deleteContact() {
+        console.log("delete");
+    }
+
+    searchGrid(value){
+        this.searchText=value;
+        /*console.log(this.searchText);
+        this.iteratorI++;
+        console.log("updateing");
+        this.rowData.push({name: "Tarsdfdasf", address: "Vijay asdfdsafNagar", number: "+9156sdf8985", email: "tar@dfsafupd"});
+        this.testTwoWay="new"+this.iteratorI;*/
+
+    }
+    compareDetails(valueA,valueB,rowA,rowB,inverter) {
+        if(rowA.data.name.toLowerCase()>rowB.data.name.toLowerCase()){
+            return 1;
         }
+        else{
+            return -1;
+        }
+
+    }
 }
